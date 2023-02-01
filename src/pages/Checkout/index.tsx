ec062127d15d45e12e6461cfc51085ac1f426318
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-01-31 21:12:51 -0300

    feat: checkout page left side skeleton

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,76 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+
+import {
+  CheckoutContainer,
+  AddressContainer,
+  PaymentContainer,
+  AddressHeader,
+  AddressInputs,
+  PaymentHeader,
+  PaymentOptions,
+} from './styles'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <CheckoutContainer>
+      <main>
+        <h2>Complete o seu pedido</h2>
+
+        <AddressContainer>
+          <AddressHeader>
+            <MapPinLine />
+            <p>Endereço de Entrega</p>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </AddressHeader>
+
+          <AddressInputs>
+            <input type="text" placeholder="CEP" />
+            <input type="text" placeholder="Rua" />
+            <input type="text" placeholder="Número" />
+            <input type="text" placeholder="Complemento" />
+            <input type="text" placeholder="Opcional" />
+            <input type="text" placeholder="Bairro" />
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" />
+          </AddressInputs>
+        </AddressContainer>
+
+        <PaymentContainer>
+          <PaymentHeader>
+            <CurrencyDollar />
+            <p>Pagamento</p>
+            <p>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </p>
+          </PaymentHeader>
+
+          <PaymentOptions>
+            <span>
+              <CreditCard />
+              <p>CARTÃO DE CRÉDITO</p>
+            </span>
+
+            <span>
+              <Bank />
+              <p>CARTÃO DE DÉBITO</p>
+            </span>
+
+            <span>
+              <Money />
+              <p>CARTÃO DE DINHEIRO</p>
+            </span>
+          </PaymentOptions>
+        </PaymentContainer>
+      </main>
+      <aside>
+        <h2>Cafés selecionados</h2>
+      </aside>
+    </CheckoutContainer>
+  )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const CheckoutContainer = styled.form`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr auto;
+`
+
+export const AddressContainer = styled.div``
+
+export const PaymentContainer = styled.div``
+
+export const AddressHeader = styled.div``
+
+export const AddressInputs = styled.div``
+
+export const PaymentHeader = styled.div``
+
+export const PaymentOptions = styled.div``
