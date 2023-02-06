481a18e4a4ada265a80cfa47fd1d7b042017999a
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-02-06 20:42:24 -0300

    feat: success page skeleton

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,65 @@
+import { MapPin, Timer } from 'phosphor-react'
+
+import successIllustration from '../../assets/success_illustration.svg'
+import {
+  SuccessContainer,
+  InfoContainer,
+  InfoItem,
+  IconContainer,
+} from './styles'
+
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <>
+      <SuccessContainer>
+        <div>
+          <h2>Uhu! Pedido confirmado</h2>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+
+          <InfoContainer>
+            <InfoItem>
+              <IconContainer>
+                <MapPin />
+              </IconContainer>
+              <div>
+                <p>
+                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                </p>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </div>
+            </InfoItem>
+
+            <InfoItem>
+              <IconContainer>
+                <Timer />
+              </IconContainer>
+              <div>
+                <p>Previsão de entrega</p>
+                <p>
+                  <strong>20 min - 30 min</strong>
+                </p>
+              </div>
+            </InfoItem>
+
+            <InfoItem>
+              <IconContainer>
+                <MapPin />
+              </IconContainer>
+              <div>
+                <p>Pagamento na entrega</p>
+                <p>
+                  <strong>Cartão de Crédito</strong>
+                </p>
+              </div>
+            </InfoItem>
+          </InfoContainer>
+        </div>
+
+        <img
+          src={successIllustration}
+          alt="Ilustração de um motoboy levando o pedido para entregar"
+        />
+      </SuccessContainer>
+    </>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const SuccessContainer = styled.main``
+
+export const InfoContainer = styled.main``
+
+export const InfoItem = styled.main``
+
+export const IconContainer = styled.main``
