c0c2254c27ca911c4d2be7363bfe1c1d5e1f7a97
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-02-06 21:22:59 -0300

    feat: success page style

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Timer } from 'phosphor-react'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 import successIllustration from '../../assets/success_illustration.svg'
 import {
@@ -6,6 +6,10 @@ import {
   InfoContainer,
   InfoItem,
   IconContainer,
+  InfoContainerGradientBorder,
+  AddressIconContainer,
+  PaymentIconContainer,
+  TimeIconContainer,
 } from './styles'
 
 export function Success() {
@@ -16,43 +20,45 @@ export function Success() {
           <h2>Uhu! Pedido confirmado</h2>
           <p>Agora é só aguardar que logo o café chegará até você</p>
 
-          <InfoContainer>
-            <InfoItem>
-              <IconContainer>
-                <MapPin />
-              </IconContainer>
-              <div>
-                <p>
-                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
-                </p>
-                <p>Farrapos - Porto Alegre, RS</p>
-              </div>
-            </InfoItem>
+          <InfoContainerGradientBorder>
+            <InfoContainer>
+              <InfoItem>
+                <AddressIconContainer>
+                  <MapPin weight="fill" size={16} />
+                </AddressIconContainer>
+                <div>
+                  <p>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  </p>
+                  <p>Farrapos - Porto Alegre, RS</p>
+                </div>
+              </InfoItem>
 
-            <InfoItem>
-              <IconContainer>
-                <Timer />
-              </IconContainer>
-              <div>
-                <p>Previsão de entrega</p>
-                <p>
-                  <strong>20 min - 30 min</strong>
-                </p>
-              </div>
-            </InfoItem>
+              <InfoItem>
+                <TimeIconContainer>
+                  <Timer weight="fill" size={16} />
+                </TimeIconContainer>
+                <div>
+                  <p>Previsão de entrega</p>
+                  <p>
+                    <strong>20 min - 30 min</strong>
+                  </p>
+                </div>
+              </InfoItem>
 
-            <InfoItem>
-              <IconContainer>
-                <MapPin />
-              </IconContainer>
-              <div>
-                <p>Pagamento na entrega</p>
-                <p>
-                  <strong>Cartão de Crédito</strong>
-                </p>
-              </div>
-            </InfoItem>
-          </InfoContainer>
+              <InfoItem>
+                <PaymentIconContainer>
+                  <CurrencyDollar weight="fill" size={16} />
+                </PaymentIconContainer>
+                <div>
+                  <p>Pagamento na entrega</p>
+                  <p>
+                    <strong>Cartão de Crédito</strong>
+                  </p>
+                </div>
+              </InfoItem>
+            </InfoContainer>
+          </InfoContainerGradientBorder>
         </div>
 
         <img
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,9 +1,85 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.main``
+export const SuccessContainer = styled.main`
+  max-width: 1120px;
+  margin: 80px auto 0 auto;
 
-export const InfoContainer = styled.main``
+  display: flex;
+  align-items: flex-end;
 
-export const InfoItem = styled.main``
+  h2 {
+    color: ${(props) => props.theme['yellow-700']};
+    font-size: 2rem;
+    font-weight: 900;
+    font-family: 'Baloo 2', sans-serif;
+    line-height: 130%;
+  }
 
-export const IconContainer = styled.main``
+  p {
+    font-size: 1.25rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['gray-800']};
+  }
+
+  img {
+    margin: 0 auto;
+  }
+`
+
+export const InfoContainerGradientBorder = styled.div`
+  margin-top: 40px;
+  padding: 1px;
+  border-radius: 6px 36px 6px 36px;
+
+  background-image: linear-gradient(150deg, #dbac2c, #8047f8);
+`
+
+export const InfoContainer = styled.div`
+  overflow: hidden;
+  border-radius: 6px 36px 6px 36px;
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  background: ${(props) => props.theme['gray-100']};
+`
+
+export const InfoItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  p {
+    font-size: 1rem;
+    color: ${(props) => props.theme['gray-700']};
+    line-height: 130%;
+  }
+
+  svg {
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const IconContainer = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const AddressIconContainer = styled(IconContainer)`
+  background-color: ${(props) => props.theme['purple-500']};
+`
+
+export const TimeIconContainer = styled(IconContainer)`
+  background-color: ${(props) => props.theme['yellow-500']};
+`
+
+export const PaymentIconContainer = styled(IconContainer)`
+  background-color: ${(props) => props.theme['yellow-700']};
+`
